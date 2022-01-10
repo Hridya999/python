7f98c4ae05142fdd437e67c51ad7c5643f9d0fc5
Hridya999 <97447453+Hridya999@users.noreply.github.com>
2022-01-10 22:04:26 +0530

    Update program17.py

--- co1/program17.py
+++ co1/program17.py
@@ -1,6 +1,6 @@
 #Sort dictionary in ascending and descending order
 
-dict={"Aswin":7,"Vyshnav":21,"Lijas":2,"Binshad":8,"Murshid":12,"Gokul":1,"Vishnu":18,"Nideesh":17,"Niranjan":5}
+dict={"Aparna":7,"hridya":21,"megha":2,"nidhina":8,"nandhana":12,"anusree":1,"reema":18,"athira":17,"vismaya":5}
 l1=list(dict.items())
 l1.sort()
 print("Dictionary in ascending order : ",l1)
