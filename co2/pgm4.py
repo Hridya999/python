484a07524e1fa14943e4b8c792e7c85a91f0fe9d
Hridya999 <97447453+Hridya999@users.noreply.github.com>
2022-01-10 22:10:18 +0530

    Update pgm4.py

--- co2/pgm4.py
+++ co2/pgm4.py
@@ -20,12 +20,3 @@ for i in range(start,end+1):
 
 print("The list of perfect square numbers are:",perfect)
 
-# method 2
-# import math
-# for i in range(1000,10000):
-#     if math.sqrt(i)%1==0:
-#         if int(i%10)%2==0:
-#             if int((i/10)%10)%2==0:
-#                 if int((i/100)%10)%2==0:
-#                     if int((i/1000)%10)%2==0:
-#                         print(i)
