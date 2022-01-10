94af45ae47b2dc39d0caf23f835e81e3087c1dd8
Hridya999 <97447453+Hridya999@users.noreply.github.com>
2022-01-10 22:09:03 +0530

    Update program8.py

--- co1/program8.py
+++ co1/program8.py
@@ -5,10 +5,3 @@ char = main_string[0]
 main_string = main_string.replace(char,'$')
 print(char+main_string[1:])
 
-# method2
-# word1=str(input("enter a word:"))
-# word2=word1
-# new=word2[0]
-# word2=word2.replace(new,"$")
-# word2=new+word2[1:]
-# print(word2)
